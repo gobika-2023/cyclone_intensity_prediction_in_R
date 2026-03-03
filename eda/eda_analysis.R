6e386cd1201665a2925ab0cee750281633e55884
Tejasreevalleru <vallerutejasree@gmail.com>
2026-03-04 00:42:54 +0530

    Update eda_analysis.R

--- eda/eda_analysis.R
+++ eda/eda_analysis.R
@@ -1,1 +1,24 @@
+# Load dataset
+data <- read.csv("data/clean_dataset.csv")
 
+# Year-wise cyclone count
+year_count <- table(data$Year)
+
+# Trend graph
+plot(year_count,
+     type="b",
+     col="blue",
+     main="Year-wise Cyclone Trend",
+     xlab="Year",
+     ylab="Number of Cyclones")
+
+# Boxplot
+boxplot(data$Feature1,
+        main="Boxplot of Feature1",
+        col="orange")
+
+# Histogram
+hist(data$Feature1,
+     main="Distribution of Feature1",
+     col="green",
+     xlab="Feature1")
